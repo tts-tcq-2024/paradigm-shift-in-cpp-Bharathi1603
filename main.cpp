4acab0a702f59538faa133c17709a6d32c1e86c3
Bharathi1603 <bharathikec013@gmail.com>
2024-07-21 15:43:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main()
     BatterySystem battery(2.26, 76.0, 0.76);
     assert(battery.isOk() == true);
     std::cout <<"Battery status: " << battery.getStateStatus() <<"\n";
+
+    battery.setTemperature(2.25);
+    std::cout <<"Battery status: " << battery.getStateStatus() <<"\n";
+
+    battery.setTemperature(2.26);
+    battery.setChargeState(76.01);
+    std::cout <<"Battery status: " << battery.getStateStatus() <<"\n";
     
     battery.setTemperature(2.26);
     battery.setChargeState(76.00);
